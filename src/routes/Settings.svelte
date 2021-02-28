c1b2941818afd24a2c9bad40ea5a012fb07e435e
James Smith <metal.lunchbox@gmail.com>
2021-02-28 21:16:35 +0800

    better theme switching in settings

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -1,10 +1,14 @@
 <script>
  import { theme } from '$components/themes/ThemeLoader.svelte'
+
+ const systemTheme = matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light'
+ const actualTheme = $theme || systemTheme
 </script>
 
 <h2>Settings</h2>
 <p>Change your personal settings for experiencing the site.</p>
 
 <h3>Light/Dark Mode</h3>
-<p>Currently using {$theme} theme.</p>
-<button on:click={theme.toggle}>Toggle theme</button>
+
+<p>Your browser or OS preferences are for {systemTheme} colors, but you can override this.</p>
+<button on:click={theme.toggle}>Switch to {actualTheme === 'dark' ? 'light' : 'dark'} theme</button>
